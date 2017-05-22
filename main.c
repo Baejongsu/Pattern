21ecb2474a7171dfca796bf51193881de2d4aca0
PH\HOON <anrmrdowk@gmail.com>
2017-05-22 16:44:55 +0900

    다없앰

--- main.c
+++ main.c
@@ -1,10 +1,5 @@
 #include <stdio.h>
 
 void main(){
-<<<<<<< HEAD
-  printf("쿄쿄쿄쿄kjhiyhgh");
-=======
-  printf("쿄쿄쿄쿄ㅎㅎ");
-  printf("하나 더 추가");
->>>>>>> ebe217eaca850bce8ed2dcbe90f2d844b86f95be
+ printf("다 없애고 추가한거");
 }
